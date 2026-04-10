95a5c9a06a26f7ad666a1005492d23838d967a2a
Scott Ott <scottott@microsoft.com>
2026-04-09 18:43:08 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import express from "express";
+import { Request, Response } from "express";
+import { ok } from "node:assert";
+import { api_config } from "./config.js";
+import './config.js';
+
+const app = express();
+const HTTP_PORT = 8080;
+
+function handlerReadiness(req: Request, res: Response) {
+  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("charset", "utf-8");
+  res.status(200);
+  res.send("OK");
+}
+
+function middlewareLogResponses(req: Request, res: Response, next: () => void) {
+  res.on("finish", () => {
+    console.log(`${req.method} ${req.originalUrl} - ${res.statusCode}`);
+    if (res.statusCode !== 200) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+    }
+  });
+  next();
+}
+
+function showHits(req: Request, res: Response, next: () => void) {
+  // insert the number of hits to the file server in the response
+  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("charset", "utf-8");
+  res.status(200);
+  res.send(`Hits: ${api_config.fileserverHits}`);
+
+  next();
+}
+
+function resetHits(req: Request, res: Response, next: () => void) {
+  api_config.fileserverHits = 0;
+  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("charset", "utf-8");
+  res.status(200);
+  res.send("Hits reset to 0");
+
+  next();
+}
+
+function middlewareMetricsInc(req: Request, res: Response, next: () => void) {
+  api_config.fileserverHits += 1;
+  
+  next();
+}
+
+app.get("/healthz", handlerReadiness);
+
+app.get("/metrics", showHits);
+app.get("/reset", resetHits);
+
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
+  app.listen(HTTP_PORT, () => {
+    console.log(`Server is running at http://localhost:${HTTP_PORT}`);
+
+    app.use(middlewareLogResponses);
+
+});
